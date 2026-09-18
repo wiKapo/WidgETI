cd5f025955e26df98ef5d24180f7a995be23ff3c
wiKapo <wiktor200polanowski@gmail.com>
2026-09-18 23:17:39 +0200

    Dodanie tytułów do dialogów wyboru czasu i daty

--- app/src/main/java/com/wikapo/widgeti/ui/EditLessonScreen.kt
+++ app/src/main/java/com/wikapo/widgeti/ui/EditLessonScreen.kt
@@ -1,21 +1,27 @@
 package com.wikapo.widgeti.ui
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -32,9 +38,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wikapo.widgeti.R
@@ -71,22 +79,30 @@ fun EditLessonScreen(
         modifier = Modifier
             .fillMaxSize()
             .imePadding()
-            .verticalScroll(scrollState)
-    ) {
+            .padding(top = 6.dp)
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
         TextField(
             state = rememberTextFieldState(initialText = lesson.name),
             label = { Text(text = stringResource(R.string.name)) },
         )
         Row {
             TextField(
-                modifier = Modifier.width(75.dp),
+                modifier = Modifier.width(100.dp),
                 state = rememberTextFieldState(initialText = lesson.type.toString()),
                 label = { Text(text = stringResource(R.string.type)) },
             )
             TextField(
-                modifier = Modifier.width(75.dp),
+                modifier = Modifier.width(100.dp),
                 state = rememberTextFieldState(initialText = lesson.group.toString()),
                 label = { Text(text = stringResource(R.string.group)) })
+            TextField(
+                modifier = Modifier.width(100.dp),
+                state = rememberTextFieldState(initialText = lesson.frequency.toString()), //TODO Only numbers
+                label = { Text(text = stringResource(R.string.frequency)) },
+            )
         }
         TextField(
             state = rememberTextFieldState(initialText = lesson.teacher),
@@ -104,7 +120,8 @@ fun EditLessonScreen(
             Column(
                 modifier = Modifier
                     .clickable { openStartTimePickerDialog = true }
-                    .width(150.dp)
+                    .size(150.dp, 60.dp)
+                    .padding(horizontal = 16.5.dp, vertical = 5.dp)
             ) {
                 Text(text = stringResource(R.string.start_time))
                 Text(text = lesson.startTime.toString())
@@ -112,34 +129,39 @@ fun EditLessonScreen(
             Column(
                 modifier = Modifier
                     .clickable { openEndTimePickerDialog = true }
-                    .width(150.dp)
+                    .size(150.dp, 60.dp)
+                    .padding(horizontal = 16.5.dp, vertical = 5.dp)
             ) {
                 Text(text = stringResource(R.string.end_time))
                 Text(text = lesson.endTime.toString())
             }
         }
-        Row {
+        Row(
+            modifier = Modifier.background(
+                LocalTextSelectionColors.current.backgroundColor.copy(
+                    alpha = 0.15f
+                )
+            )
+        ) {
             Column(
                 modifier = Modifier
                     .clickable { openStartDatePickerDialog = true }
-                    .width(150.dp)
+                    .size(150.dp, 60.dp)
+                    .padding(horizontal = 16.5.dp, vertical = 5.dp)
             ) {
-                Text(text = stringResource(R.string.start_date))
+                Text(text = stringResource(R.string.start_date)) //TODO tekst wyszarzony
                 Text(text = lesson.startDate.toString())
             }
             Column(
                 modifier = Modifier
                     .clickable { openEndDatePickerDialog = true }
-                    .width(150.dp)
+                    .size(150.dp, 60.dp)
+                    .padding(horizontal = 16.5.dp, vertical = 5.dp)
             ) {
                 Text(text = stringResource(R.string.end_date))
                 Text(text = lesson.endDate.toString())
             }
         }
-        TextField(
-            state = rememberTextFieldState(initialText = lesson.frequency.toString()), //TODO Only numbers
-            label = { Text(text = stringResource(R.string.frequency)) },
-        )
         Row {
             Button(onClick = { /*TODO*/ }) {
                 Text(text = stringResource(R.string.cancel))
@@ -156,6 +178,7 @@ fun EditLessonScreen(
         openStartTimePickerDialog ->
             TimePickerDialog(
                 time = lesson.startTime,
+                title = stringResource(R.string.select_start_time),
                 onDismiss = { openStartTimePickerDialog = false },
                 onConfirm = { state ->
                     Log.d("TimePicker", "Selected time: ${state.hour}:${state.minute}")
@@ -165,6 +188,7 @@ fun EditLessonScreen(
         openEndTimePickerDialog ->
             TimePickerDialog(
                 time = lesson.endTime,
+                title = stringResource(R.string.select_end_time),
                 onDismiss = { openEndTimePickerDialog = false },
                 onConfirm = { state ->
                     Log.d("TimePicker", "Selected time: ${state.hour}:${state.minute}")
@@ -174,6 +198,7 @@ fun EditLessonScreen(
         openStartDatePickerDialog ->
             DatePickerDialog(
                 date = lesson.startDate,
+                title = stringResource(R.string.select_start_date),
                 onDateSelected = {
                     Log.d("DatePicker", "Selected date: $it")
                     openStartDatePickerDialog = false
@@ -183,6 +208,7 @@ fun EditLessonScreen(
         openEndDatePickerDialog ->
             DatePickerDialog(
                 date = lesson.endDate,
+                title = stringResource(R.string.select_end_date),
                 onDateSelected = {
                     Log.d("DatePicker", "Selected date: $it")
                     openEndDatePickerDialog = false
@@ -195,6 +221,7 @@ fun EditLessonScreen(
 @Composable
 fun TimePickerDialog(
     time: LocalTime,
+    title: String,
     onDismiss: () -> Unit,
     onConfirm: (TimePickerState) -> Unit
 ) {
@@ -205,7 +232,7 @@ fun TimePickerDialog(
         onDismissRequest = onDismiss,
         dismissButton = {
             TextButton(onClick = { onDismiss() }) {
-                Text(stringResource(R.string.dismiss))
+                Text(stringResource(R.string.cancel))
             }
         },
         confirmButton = {
@@ -216,7 +243,14 @@ fun TimePickerDialog(
             }
         },
         text = {
-            TimePicker(state = timePickerState)
+            Column {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                TimePicker(state = timePickerState)
+            }
         }
     )
 }
@@ -225,6 +259,7 @@ fun TimePickerDialog(
 @Composable
 fun DatePickerDialog(
     date: LocalDate?,
+    title: String,
     onDateSelected: (Long?) -> Unit,
     onDismiss: () -> Unit
 ) {
@@ -246,7 +281,17 @@ fun DatePickerDialog(
             }
         }
     ) {
-        DatePicker(state = datePickerState)
+        DatePicker(
+            state = datePickerState,
+            title = {
+                Text(
+                    text = title,
+                    color = DatePickerDefaults.colors().titleContentColor,
+                    modifier = Modifier.padding(
+                        PaddingValues(start = 24.dp, end = 12.dp, top = 16.dp)
+                    )
+                )
+            })
     }
 }
 
